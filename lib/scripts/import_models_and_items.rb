ae97494b0ac2f7cf17a152f237e6404780adf6f4
mradl <manuel.radl@zhdk.ch>
2024-01-26 17:27:09 +0100

    revision v5, cleanup

--- lib/scripts/import_models_and_items.rb
+++ lib/scripts/import_models_and_items.rb
@@ -33,8 +33,6 @@ end
 
 def print_csv_model_row(row)
   puts "----> print_csv_model_row "
-  # puts row['model']
-  # puts row[ModelKeys::MODEL.to_s]
   puts row[ModelKeys::MODEL]
   puts "----------"
   puts row[ModelKeys::MANUFACTURER]
@@ -49,20 +47,15 @@ def to_bool(str)
   when 'false', 'f', 'no', 'n', '0'
     false
   else
-    nil # or raise an error if you prefer
+    nil
   end
 end
 
 def gen_model_attributes(row)
-  puts "csv-row _> #{row}"
-  # puts "----"
-  #
+
   print_csv_model_row(row)
 
-  puts "---- ??? #{ModelKeys::MODEL}  "
-
   model_name = "test-#{row[ModelKeys::MODEL]}"
-  puts "model_name???: #{model_name}"
 
   model_attributes = {
     # product: row[ModelKeys::MODEL],  # TODO: revert to this
@@ -72,6 +65,7 @@ def gen_model_attributes(row)
     technical_detail: row[ModelKeys::TECHNICAL_DETAILS],
     description: row[ModelKeys::DESCRIPTION]
   }
+  return model_attributes
 end
 
 def import_models_from_csv(error_map)
@@ -84,14 +78,10 @@ def import_models_from_csv(error_map)
     rescue => e
       error_map["model/#{model_attributes[:product]}"] = { 'data': model_attributes, 'error': e.message }
     end
-
   end
 end
 
 def extract_building_name_and_code(building_name)
-  puts "building_name: #{building_name}"
-
-  # extract building.name & building.code from building_name "<building.name> (<building.code>)"
   building_name_extracted = building_name.match(/^(.*)\s\((.*)\)$/)[1]
   building_code_extracted = building_name.match(/^(.*)\s\((.*)\)$/)[2]
 
@@ -99,22 +89,13 @@ def extract_building_name_and_code(building_name)
 end
 
 def gen_item_attributes(row)
-  puts row
-  puts "----"
-
   # model_name = row[ModelKeys::MODEL]                # TODO: revert to this
   model_name = "test-#{row[ModelKeys::MODEL]}"
   puts "model_name: #{model_name}"
 
   owner_name = row[ItemKeys::OWNER]
-  puts "owner_name: #{owner_name}"
-
   is_retired = to_bool(row[ItemKeys::RETIRED])
-  puts "is_retired1: #{is_retired}"
-  puts "is_retired2: #{is_retired.class}"
-
   responsible_department_name = row[ItemKeys::RESPONSIBLE_DEPARTMENT]
-  puts "responsible_department_name: #{responsible_department_name}"
 
   building_name = row[ItemKeys::BUILDING]
   building_code_extracted, building_name_extracted = extract_building_name_and_code(building_name)
@@ -122,27 +103,11 @@ def gen_item_attributes(row)
   puts "building_code_extracted: #{building_code_extracted}"
 
   room_name = row[ItemKeys::ROOM]
-  puts "room_name: #{room_name}"
-
-  puts "----"
-
   model_rec = Model.find_by!(product: model_name)
-  puts "model_rec: #{model_rec}"
-
-  puts "owner_rec.name: ??? >#{owner_name}<"
   owner_rec = InventoryPool.find_by!(name: owner_name)
-  puts "owner_rec: #{owner_rec}"
-
   responsible_department_name_rec = InventoryPool.find_by!(name: responsible_department_name)
-  puts "responsible_department_name_rec: #{responsible_department_name_rec}"
-
   building_rec = Building.find_by!(name: building_name_extracted, code: building_code_extracted)
-  # building_name_with_code= "#{building_rec.name} (#{building_rec.code})"
-  puts "building_rec: #{building_rec}"
-  # puts "building_name_with_code: #{building_name_with_code}"
-
   room_rec = Room.find_by!(building_id: building_rec.id, name: room_name)
-  puts "building_rec: #{room_rec}"
 
   item_attributes = {
     # name: "#{row[ItemKeys::NAME]}",          # TODO: revert to this
@@ -176,9 +141,6 @@ def import_items_from_csv(error_map)
     rescue => e
       error_map["items/#{item_attributes[:name]}"] = { 'data': item_attributes, 'error': e.message }
     end
-
-    # break
-
   end
 end
 
